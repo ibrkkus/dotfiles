2a5f273af47486d95adc6b173205dc3a243602f3
ibrkkus <ibrkkus@gmail.com>
2024-01-12 16:51:45 +0300

    vscode settings

--- gather.sh
+++ gather.sh
@@ -2,3 +2,6 @@
 cp ~/.zshrc ./zshrc
 cp ~/.tmux.conf ./tmux.conf
 cp ~/.Xresources ./Xresources
+cp ~/.config/Code/User/settings.json ./vscCodeSettings.json
+cp ~/.config/Code/User/keybindings.json ./vscCodeKeybindings.json
+
